50cf013af1ca634055668bcb6975606134612804
stewie <stewiemayer@hotmail.com>
2025-10-08 17:00:20 -0600

    feat: se modifican las rutas para utilizar los controladores

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,28 +1,24 @@
 import { Router } from "express";
+import {
+  cancelSubscription,
+  createSubscription,
+  deleteSubscription,
+  getSubscription,
+  getSubscriptions,
+  getUserSubscriptions,
+  updateSubscription,
+} from "../controllers/subscription.controller.js";
+import authorize from "../middleware/auth.middleware.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get("/", (req, res) =>
-  res.send({ title: "GET all subscriptions" })
-);
-subscriptionRouter.get("/:id", (req, res) =>
-  res.send({ title: "GET subscription details" })
-);
-subscriptionRouter.post("/", (req, res) =>
-  res.send({ title: "CREATE subscription" })
-);
-subscriptionRouter.put("/:id", (req, res) =>
-  res.send({ title: "UPDATE subscription" })
-);
-subscriptionRouter.delete("/:id", (req, res) =>
-  res.send({ title: "DELETE subscription" })
-);
-subscriptionRouter.get("/user/:id", (req, res) =>
-  res.send({ title: "GET all user subscriptions" })
-);
-subscriptionRouter.put("/:id/cancel", (req, res) =>
-  res.send({ title: "CANCEL subscription" })
-);
+subscriptionRouter.get("/", getSubscriptions);
+subscriptionRouter.get("/:id", getSubscription);
+subscriptionRouter.post("/", authorize, createSubscription);
+subscriptionRouter.put("/:id", authorize, updateSubscription);
+subscriptionRouter.delete("/:id", authorize, deleteSubscription);
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
+subscriptionRouter.put("/:id/cancel", authorize,cancelSubscription);
 subscriptionRouter.get("/upcoming-renewals", (req, res) =>
   res.send({ title: "GET upcoming renewals" })
 );
